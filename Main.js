33718ee36a80cc955501501b816a1763add31faa
ega <radiegtya@yahoo.co.id>
2017-02-13 20:38:27 +0700

    router fix

--- Main.js
+++ Main.js
@@ -1,31 +1,32 @@
 import React, { Component } from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {Actions, Scene, Router, Switch, Modal} from 'react-native-router-flux';
+import About from './scenes/About';
+import Question from './scenes/Question';
 
-class Main extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-      </View>
-    );
-  }
+class TabIcon extends Component {
+    render(){
+      const title = this.props.title;
+      return (
+        <Text>{title}</Text>
+      );
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-});
+class Main extends Component {
+  componentWillMount() {
+    this.scenes = Actions.create(
+      <Scene key="root" tabs={true}>
+        <Scene key="tabbar" tabs={true} tabBarStyle={{backgroundColor:'#f7f7f7'}}>
+          <Scene key="Questions" component={Question} title="Questions" icon={TabIcon} hideNavBar={true}/>
+          <Scene key="About" component={About} title="About" icon={TabIcon} hideNavBar={true}/>
+        </Scene>
+      </Scene>
+    );
+  }
+  render() {
+    return <Router scenes={this.scenes}/>
+  }
+}
 
 module.exports = Main;

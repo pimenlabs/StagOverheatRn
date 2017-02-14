23690160ded733435c2d97c1ce6d023f948ee9fb
ega <radiegtya@yahoo.co.id>
2017-02-14 14:22:11 +0700

    chap 5 flex dan style

--- scenes/Question.js
+++ scenes/Question.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, ListView} from 'react-native';
 
 export default class Question extends Component {
+
+  constructor() {
+    super();
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.questions = [
+      {title: "First Question", author: "Feri"},
+      {title: "Second Question", author: "Donald"},
+      {title: "Third Question", author: "Heisenberg"},
+    ];
+    this.state = {
+      dataSource: ds.cloneWithRows(this.questions),
+    };
+  }
+
+  renderRow(rowData){
+    return (
+      <View style={styles.row}>
+        <View style={styles.titleRow}>
+          <Text>{rowData.title}</Text>
+        </View>
+        <View style={styles.authorRow}>
+          <Text>{rowData.author}</Text>
+        </View>
+      </View>
+    );
+  }
+
   render(){
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Question Page
-        </Text>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={this.renderRow.bind(this)}
+        />
       </View>
     );
   }
@@ -17,12 +45,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  row: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 20,
+    marginBottom: 10,
+    backgroundColor: '#FFF',
+    flexDirection: "row"
   },
+  titleRow: {
+    flex: 4,
+  },
+  authorRow: {
+    flex: 1
+  }
 });
